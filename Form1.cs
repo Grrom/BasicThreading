d4b20d0fc3e6d02180fdc7bcfd76ed7f0a87bd1f
Jerome <josejeromelalunio@gmail.com>
2021-11-09 16:57:40 +0800

    complete na GGEZ

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,5 +18,23 @@ namespace BasicThreading
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "     - Thread Running -";
+            Thread threadA = new Thread(MyThreadClass.Thread1);
+            Thread threadB = new Thread(MyThreadClass.Thread1);
+
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            System.Diagnostics.Debug.WriteLine("- End of Thread");
+            label1.Text = "      - End Of Thread -";
+        }
     }
 }
